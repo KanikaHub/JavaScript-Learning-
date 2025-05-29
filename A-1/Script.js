24087ee3e1fc23a97bc14cdf5dd290bd08c9c658
Kanika <vermakanika.00@gmail.com>
2025-05-29 16:52:48 +0530

    this is javascript learning

---
+++ A-1/Script.js
@@ -0,0 +1,44 @@
+let a= 20;
+let b=10;
+let result=a+b;
+console.log(result);
+
+
+let x= 12;
+let y=20;
+let final= (x % 10 == 0 ||  y% 10 == 0);
+console.log(final);
+
+
+let C = 5;
+let D = 3;
+let answer = C < 10 && C > D;
+console.log(answer);
+
+
+let N = 4567;
+let firstDigit = Math.floor(N / 1000);
+console.log(firstDigit);
+
+
+let P = 4567;
+let lastDigit = P % 10;
+console.log(lastDigit);
+
+
+let A = 3; 
+let B = 10; 
+let remainder = B % A;
+console.log(remainder);
+
+let c= 4;
+let d = 6;
+console.log(c*d);
+
+let A1 = 50;
+let B1 = 20;
+let C1 = 100;
+let total = A1 + B1 + C1;
+let average = (total / 3).toFixed(2);
+console.log(total);   
+console.log(average);
